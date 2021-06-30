eeb2bdafec9b3feba4cbd1b20917dad07ca84219
Saradev05 <devyanand@gmail.com>
2021-06-30 18:45:09 +0000

    falta retoquitos

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
+import LogoIMG from "../../img/logo.png";
 import "../../styles/navbar.scss";
 
 export const Navbar = () => {
@@ -10,10 +11,7 @@ export const Navbar = () => {
 		<nav className="navbarcss navbar ">
 			<Link to="/">
 				<span className="navb ar-brand mb-0 h1">
-					<img
-						className="logocss"
-						src={"https://logos-marcas.com/wp-content/uploads/2020/11/Star-Wars-Logo.png "}
-					/>
+					<img className="logocss" src={LogoIMG} />
 				</span>
 			</Link>
 			<div className="ml-auto dropdown">
@@ -28,6 +26,16 @@ export const Navbar = () => {
 				</button>
 				<ul className="dropdown-menu" aria-labelledby="favorite">
 					{store.favorites.map((favorite, i) => {
+						// const setObj = new Set(); // creamos pares de clave y array
+
+						// const unicFab = store.favorites.reduce((acc, favorite) => {
+						// 	if (!setObj.has(favorite.name)) {
+						// 		setObj.add(favorite.name, favorite);
+						// 		acc.push(favorite);
+						// 	}
+						// 	return acc;
+						// }, []);
+
 						return (
 							<li key={i} className="dropdown-item">
 								{favorite}
@@ -46,3 +54,5 @@ export const Navbar = () => {
 		</nav>
 	);
 };
+
+//no repetir favorite https://www.neoguias.com/como-eliminar-duplicados-array-javascript/
